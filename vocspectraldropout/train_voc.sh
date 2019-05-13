5fefdb472bdd6678f860170d0813ebfac328c8a2
Sam Spilsbury <smspillaz@gmail.com>
2019-05-13 16:48:34 +0300

    train_voc: Add ability to specify experiment

--- vocspectraldropout/train_voc.sh
+++ vocspectraldropout/train_voc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+EXPERIMENT=${EXPERIMENT:-default}
+
 python train_semantic_segmentation.py \
     --cuda \
     --source-images ../data/VOCdevkit/VOC2012/JPEGImages \
@@ -7,4 +9,7 @@ python train_semantic_segmentation.py \
     --training-set ../data/VOCdevkit/VOC2012/ImageSets/Segmentation/train.txt \
     --validation-set ../data/VOCdevkit/VOC2012/ImageSets/Segmentation/val.txt \
     --test-set ../data/VOCdevkit/VOC2012/ImageSets/Segmentation/trainval.txt \
-    --save-to $@
+    --save-to experiments/$EXPERIMENT/model.pt \
+    --log-statistics experiments/$EXPERIMENT/logs/statistics \
+    --save-interesting-images experiments/$EXPERIMENT/logs/interesting \
+    $@
